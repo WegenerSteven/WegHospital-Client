d63151e24d136045574073ff021c7b41652d2bca
WegenerSteven <wegenersteven580@gmail.com>
2025-07-05 07:25:51 +0300

    implement toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { Toaster } from 'sonner'
 
 import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
+import { AuthProvider } from './contexts/AuthContext'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
@@ -35,9 +37,12 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <TanStackQueryProvider.Provider>
-        <RouterProvider router={router} />
-      </TanStackQueryProvider.Provider>
+      <AuthProvider>
+        <TanStackQueryProvider.Provider>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" />
+        </TanStackQueryProvider.Provider>
+      </AuthProvider>
     </StrictMode>,
   )
 }
