67350a885d05e8ccdd0b82f572ed1c7702efbc95
WegenerSteven <wegenersteven580@gmail.com>
2025-07-05 07:27:29 +0300

    implement sidebar dashboard layout

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,14 +1,17 @@
 import SideNav from './SideNav';
 import { Outlet } from '@tanstack/react-router';
+import ProtectedRoute from './ProtectedRoute';
 
 const DashboardLayout = () => {
     return (
-        <div className='flex flex-col lg:flex-row min-h-screen'>
-            <SideNav/>
-            <main className='flex-1 bg-gray-50 lg:ml-0 pt-16 lg:pt-0'>
-                <Outlet/>
-            </main>
-        </div>
+        <ProtectedRoute>
+            <div className='flex flex-col lg:flex-row min-h-screen'>
+                <SideNav/>
+                <main className='flex-1 bg-gray-50 lg:ml-0 pt-16 lg:pt-0'>
+                    <Outlet/>
+                </main>
+            </div>
+        </ProtectedRoute>
     );
 }
 
